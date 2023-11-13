b7223b0f96a00909f69add71109f6adc599261c5
Gustavo Alfredo <111892331+Gustavocorrea0@users.noreply.github.com>
2023-11-13 16:22:14 -0300

    programacao-iii-aula-30-10-2023-main directory

--- programacao-iii-aula-30-10-2023-main/src/main/java/br/edu/umfg/exemplomigrations/exemplomigrations/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.umfg.exemplomigrations.exemplomigrations.repository;
-
-import br.edu.umfg.exemplomigrations.exemplomigrations.model.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepository extends CrudRepository<Client, Long> {
-
-}
