c84551dcf487220c24a685dbb6dc5673aeb695a0
Gustavo Alfredo <111892331+Gustavocorrea0@users.noreply.github.com>
2023-11-13 16:39:34 -0300

    Delete programacao-iii-aula-30-10-2023-main directory

--- programacao-iii-aula-30-10-2023-main/src/main/java/br/edu/umfg/exemplomigrations/exemplomigrations/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.umfg.exemplomigrations.exemplomigrations.repository;
-
-import br.edu.umfg.exemplomigrations.exemplomigrations.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
